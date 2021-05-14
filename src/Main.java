b153b9b8b6acf59dd9e6939f3c4a2c0fcb7387a7
Artem <kolaguin@gmail.com>
2021-05-14 12:26:08 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int cost = 999;
         int rubForBonusMile = 20;
-        int bonusMile = cost/rubForBonusMile;
+        int bonusMile = cost / rubForBonusMile;
         System.out.println(bonusMile);
     }
 }
