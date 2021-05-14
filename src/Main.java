b1b8652f7592c7c91b741f9be8a58d74cfafb9a7
Artem <kolaguin@gmail.com>
2021-05-14 11:52:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 999;
+        int rubForBonusMile = 20;
+        int bonusMile = cost/rubForBonusMile;
+        System.out.println(bonusMile);
+    }
+}
